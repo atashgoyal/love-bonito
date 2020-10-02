b47000be20419f5db1aa2a3f80734415012a670e
Atash Goyal <Atash.Goyal@zettalabs.com>
2020-10-02 15:33:00 +0530

    dist commit for service workers.

---
+++ dist/precache-manifest.3bb95dbb99d4994895f3f1b37db03308.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41399adb8efcc35c633",
+    "url": "/css/app.0b725f67.css"
+  },
+  {
+    "revision": "b193427d5b24ed87adfe",
+    "url": "/css/chunk-vendors.6a7c8892.css"
+  },
+  {
+    "revision": "09cce4af29b3c9bca7b63ea94f0f43c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41399adb8efcc35c633",
+    "url": "/js/app.e8ed74ba.js"
+  },
+  {
+    "revision": "b193427d5b24ed87adfe",
+    "url": "/js/chunk-vendors.64c2c99f.js"
+  },
+  {
+    "revision": "6096244480a1d061f1f3b6077c68d81e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
