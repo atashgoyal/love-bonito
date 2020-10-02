74deda09443f0af9acad5358b077f0bc26601552
Atash Goyal <Atash.Goyal@zettalabs.com>
2020-10-02 19:45:53 +0530

    challenge 4 performance close to 100 in lighthouse

---
+++ dist/precache-manifest.e3314bce51ceb00f2a145f15fbdc868b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc5943747e15b2ff0f2",
+    "url": "/css/app.f2ab7a67.css"
+  },
+  {
+    "revision": "b193427d5b24ed87adfe",
+    "url": "/css/chunk-vendors.6a7c8892.css"
+  },
+  {
+    "revision": "9e3ecee24a347146b18d8de474d68874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc5943747e15b2ff0f2",
+    "url": "/js/app.4c11573f.js"
+  },
+  {
+    "revision": "b193427d5b24ed87adfe",
+    "url": "/js/chunk-vendors.64c2c99f.js"
+  },
+  {
+    "revision": "6096244480a1d061f1f3b6077c68d81e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
