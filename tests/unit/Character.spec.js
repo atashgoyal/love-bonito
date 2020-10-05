be326f4899da83fef84a92cb5389a8fe2bafbe6c
Atash Goyal <Atash.Goyal@zettalabs.com>
2020-10-05 16:11:40 +0530

    test cases.

---
+++ tests/unit/Character.spec.js
@@ -0,0 +1,11 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import Character from '@/components/Character'
+
+describe('Character', () => {
+    let wrapper;
+
+    it('renders the component', async () => {
+    })
+
+})
+
